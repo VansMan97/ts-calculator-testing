0e75c3c7fb2f60361debf8c77162a7d46d0eaa19
VansMan97 <vansman97@gmail.com>
2022-09-20 15:51:27 +0200

    1

---
+++ Calculator.ts
@@ -0,0 +1,21 @@
+class Calculator{
+    a: number;
+    b: number;
+        constructor(a:number, b:number){
+          this.a=a;
+          this.b=b;
+        }
+        add(a: number, b: number): number {
+            return a + b;
+        }
+        subtract(a: number, b: number): number {
+            return a - b;
+        }
+        multiple(a: number, b: number): number {
+            return a * b;
+        }
+        divide(a: number, b: number): number {
+            return a/b;
+        }
+        } 
+
