5654aef3217c300ebe41ed872004403768ba2b6f
VansMan97 <vansman97@gmail.com>
2022-09-21 15:42:04 +0200

    Update Calculator.test.ts

--- src/Calculator.test.ts
+++ src/Calculator.test.ts
@@ -1,7 +1,7 @@
 import  Calculator  from './Calculator'; 
 
 
-describe('BasicCalulator Testing', () => {
+describe('Calulator Testing', () => {
   let calc: Calculator = new Calculator(4,5,"+");
 test('4 + 5 = 9', () => {
     expect(calc.Calculate()).toEqual(9);
